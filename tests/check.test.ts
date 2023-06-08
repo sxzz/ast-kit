3f687b7b7d5cc257bf7911ba6607c6fb69f20c3c
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-06-09 04:52:37 +0800

    test: tidy

---
+++ tests/check.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, test } from 'vitest'
+import { isLiteralType, isTypeOf } from '../src'
+
+describe('utils', () => {
+  test('isTypeOf', () => {
+    expect(isTypeOf({ type: 'NullLiteral' }, ['NullLiteral'])).toBe(true)
+    expect(isTypeOf({ type: 'NullLiteral' }, ['Literal'])).toBe(true)
+    expect(isTypeOf({ type: 'AnyTypeAnnotation' }, ['NullLiteral'])).toBe(false)
+  })
+
+  test('isLiteralType', () => {
+    expect(isLiteralType({ type: 'NullLiteral' })).toBe(true)
+    expect(isLiteralType({ type: 'AnyTypeAnnotation' })).toBe(false)
+  })
+})
--- tests/resolve.test.ts
+++ tests/resolve.test.ts
@@ -1,54 +1,30 @@
 import { describe, expect, test } from 'vitest'
-import {
-  type ExpressionStatement,
-  type MemberExpression,
-  type Program,
-} from '@babel/types'
-import { babelParse, resolveIdentifier } from '../src'
+import { type MemberExpression } from '@babel/types'
+import { babelParseExpression, resolveIdentifier } from '../src'
 
-function getExpression(program: Program, index: number) {
-  return (program.body[index] as ExpressionStatement).expression
+function parse(code: string) {
+  return babelParseExpression(code, undefined, {
+    errorRecovery: true,
+  }) as MemberExpression
 }
 
 describe('resolve', () => {
   test('resolveIdentifier', () => {
-    const program = babelParse(
-      `
-    foo.bar.baz
-    foo.bar["baz"]
-    foo.bar[b];
-    this.#a
-    super.#a
-    `,
-      undefined,
-      { errorRecovery: true }
+    expect(resolveIdentifier(parse('foo.bar.baz'))).toEqual([
+      'foo',
+      'bar',
+      'baz',
+    ])
+    expect(resolveIdentifier(parse('foo.bar["baz"]'))).toEqual([
+      'foo',
+      'bar',
+      'baz',
+    ])
+    expect(resolveIdentifier(parse('this.#a'))).toEqual(['this', '#a'])
+    expect(resolveIdentifier(parse('super.#a'))).toEqual(['super', '#a'])
+
+    expect(() => resolveIdentifier(parse('foo.bar[b]'))).toThrow(
+      'Invalid Identifier'
     )
-    // @ts-expect-error
-    expect(() => resolveIdentifier(program)).toThrow()
-
-    expect(() =>
-      // @ts-expect-error
-      resolveIdentifier(program.body[0])
-    ).toThrow('Invalid Identifier')
-
-    expect(
-      resolveIdentifier(getExpression(program, 0) as MemberExpression)
-    ).toEqual(['foo', 'bar', 'baz'])
-
-    expect(
-      resolveIdentifier(getExpression(program, 1) as MemberExpression)
-    ).toEqual(['foo', 'bar', 'baz'])
-
-    expect(() =>
-      resolveIdentifier(getExpression(program, 2) as MemberExpression)
-    ).toThrow('Invalid Identifier')
-
-    expect(
-      resolveIdentifier(getExpression(program, 3) as MemberExpression)
-    ).toEqual(['this', '#a'])
-
-    expect(
-      resolveIdentifier(getExpression(program, 4) as MemberExpression)
-    ).toEqual(['super', '#a'])
   })
 })
