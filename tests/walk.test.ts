b6d60d30004a478ae2ba3c87053c69ea972e0cdd
LoTwT <709937065@qq.com>
2024-01-08 17:48:16 +0800

    feat: walkExportDeclaration

--- tests/walk.test.ts
+++ tests/walk.test.ts
@@ -1,8 +1,11 @@
 import { describe, expect, expectTypeOf, test, vi } from 'vitest'
 import {
+  type ExportBinding,
   type ImportBinding,
   babelParse,
+  isTypeOf,
   walkASTSetup,
+  walkExportDeclaration,
   walkImportDeclaration,
 } from '../src'
 import type * as t from '@babel/types'
@@ -119,5 +122,101 @@ describe('walk', () => {
     expect(walkFunctionDeclaration).toBeCalledTimes(2)
     expect(walkTS).toBeCalledTimes(2)
     expect(walkIdentifier).toBeCalledTimes(4)
+  })
+
+  describe('walkExportDeclaration', () => {
+    function getExports(code: string) {
+      const ast = babelParse(code, 'ts')
+
+      const exports: Record<string, ExportBinding> = {}
+
+      for (const n of ast.body) {
+        if (
+          !!n &&
+          isTypeOf(n, [
+            'ExportDefaultDeclaration',
+            'ExportAllDeclaration',
+            'ExportNamedDeclaration',
+          ])
+        ) {
+          walkExportDeclaration(exports, n)
+        }
+      }
+
+      return exports
+    }
+
+    function mapExports(exports: Record<string, ExportBinding>) {
+      return Object.values(exports).map(
+        // eslint-disable-next-line unused-imports/no-unused-vars
+        ({ declaration, specifier, ...rest }) => rest,
+      )
+    }
+
+    test.each([
+      { code: 'export default function() {}', desc: 'function' },
+      { code: 'export default function foo() {}', desc: 'function with id' },
+      { code: 'export default class {}', desc: 'class' },
+      { code: 'export default class Cls {}', desc: 'class with id' },
+      { code: 'export default function tsFoo(): void', desc: 'ts declaration' },
+      {
+        code: `
+          const one = 1
+          export default one
+        `,
+        desc: 'expression with VariableDeclaration',
+      },
+      { code: 'export default [1]', desc: 'expression' },
+    ])('ExportDefaultDeclaration $desc', ({ code }) => {
+      const exports = getExports(code)
+
+      expect(mapExports(exports)).matchSnapshot()
+    })
+
+    test.each([
+      { code: `export * from "a"`, desc: 'value' },
+      { code: `export type * from "a"`, desc: 'type' },
+    ])('ExportAllDeclaration $desc', ({ code }) => {
+      const exports = getExports(code)
+      expect(mapExports(exports)).toMatchSnapshot()
+    })
+
+    test('ExportNamedDeclaration', () => {
+      const exports = getExports(`
+        const one = 1
+        const two = 2
+        export { one, one as aliasOne, type one as specifierTypeOne }
+        export type { one as nodeTypeOne }
+        export type { two }
+  
+        export { a, b as aliasB } from "z"
+        export * as zAll from "z"
+  
+        export type { c, d as aliasTypeD } from "z"
+        export type * as zTypeAll from "z"
+      `)
+
+      expect(mapExports(exports)).toMatchSnapshot()
+    })
+
+    /**
+     * @see https://tc39.es/ecma262/multipage/ecmascript-language-scripts-and-modules.html#table-export-forms-mapping-to-exportentry-records
+     */
+    test.each([
+      { code: 'export var v', desc: '1' },
+      { code: 'export default function f() {}', desc: '2' },
+      { code: 'export default function () {}', desc: '3' },
+      { code: 'export default 42', desc: '4' },
+      { code: `const x = "5";export {x}`, desc: '5' },
+      { code: `const v = "v";export {v as x}`, desc: '6' },
+      { code: `export {x} from "mod"`, desc: '7' },
+      { code: `export {v as x} from "mod"`, desc: '8' },
+      { code: `export * from "mod"`, desc: '9' },
+      { code: `export * as ns from "mod"`, desc: '10' },
+    ])('ExportEntry record fields $desc', ({ code }) => {
+      const exports = getExports(code)
+
+      expect(mapExports(exports)).toMatchSnapshot()
+    })
   })
 })
