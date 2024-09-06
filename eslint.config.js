07bd85d1dbe543b7ed66baa2df03820bf37407e8
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-09-06 23:59:25 +0800

    chore: simplify eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,8 @@
-import { sxzz, tseslint } from '@sxzz/eslint-config'
+import { sxzz } from '@sxzz/eslint-config'
 
 export default sxzz([
   {
     files: ['src/**.ts'],
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-    },
     rules: {
       '@typescript-eslint/explicit-module-boundary-types': 'error',
     },
