090f538aaa8ec017d2a58279982176adcaa17c2e
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-08-11 12:20:36 +0800

    chore: fix type error

--- tests/_utils.ts
+++ tests/_utils.ts
@@ -1,12 +1,15 @@
 import { babelParse, babelParseExpression } from '../src'
-import type { ParseResult } from '@babel/parser'
+import type { ParseError } from '@babel/parser'
 import type * as t from '@babel/types'
 
-export function parse(code: string, expression?: false): ParseResult<t.Program>
+export function parse(
+  code: string,
+  expression?: false,
+): t.Program & { errors: null | ParseError[] }
 export function parse<T extends t.Node>(
   code: string,
   expression: true,
-): ParseResult<T>
+): T & { errors: null | ParseError[] }
 export function parse<T extends t.Node>(code: string, expression = false) {
   return (expression ? babelParseExpression<T> : babelParse)(code, undefined, {
     plugins: ['typescript', 'importAttributes'],
